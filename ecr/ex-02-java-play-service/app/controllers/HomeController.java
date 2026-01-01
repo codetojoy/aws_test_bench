1f913f77fa4d9826fa70d31b6f75e6493f7deb0f
Michael Easter <codetojoy@gmail.com>
2025-12-31 20:00:16 -0400

    server IP address

--- ecr/ex-02-java-play-service/app/controllers/HomeController.java
+++ ecr/ex-02-java-play-service/app/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package controllers;
 
 import play.mvc.*;
+import java.net.InetAddress;
 
 public class HomeController extends Controller {
 
     public Result index(Http.Request request) {
         String timestamp = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
-        String ipAddress = request.remoteAddress();
-        return ok(views.html.main.render(timestamp, ipAddress));
+        String clientIpAddress = request.remoteAddress();
+        String serverIpAddress = getServerIpAddress();
+        return ok(views.html.main.render(timestamp, clientIpAddress, serverIpAddress));
+    }
+
+    private String getServerIpAddress() {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            return localHost.getHostAddress();
+        } catch (Exception e) {
+            return "Unable to determine server IP";
+        }
     }
 
     public Result health() {
