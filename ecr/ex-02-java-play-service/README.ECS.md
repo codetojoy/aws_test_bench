2309302cf49d385122ecceb7dea92e48576b0686
Michael Easter <codetojoy@gmail.com>
2026-01-01 13:54:34 -0400

    sync with ecs/ex-06

--- ecr/ex-02-java-play-service/README.ECS.md
+++ ecr/ex-02-java-play-service/README.ECS.md
@@ -2,6 +2,7 @@
 ### Notes
 
 * after publishing image to ECR, here are manual steps for ECS
+    * this has been automated in ~/ecs/ex-06
 * create cluster
 * create task definition
     * Fargate, 0.5 vCPU, 3 GB
@@ -9,6 +10,7 @@
     * add port mapping for 9000
         * find default security group and add rule for 9000. e.g. 22.224.0.0/16
     * env var APPLICATION_SECRET with strong value from [here](https://jam.dev/utilities/random-string-generator)
+    * env var MY_FOOBAR for a test value 
 * in cluster, create task
     * go to Networking and find public IP address
     * visit http://PUBLIC_IP:9000
@@ -17,4 +19,5 @@
 
 * consider Secret Manager
 * consider Terraform to create infra: basic
+    * this has been automated in ~/ecs/ex-06
 * consider Terraform with dedicated VPC, security group, etc
--- ecr/ex-02-java-play-service/app/controllers/HomeController.java
+++ ecr/ex-02-java-play-service/app/controllers/HomeController.java
@@ -1,16 +1,34 @@
 package controllers;
 
 import play.mvc.*;
+import com.typesafe.config.Config;
+import javax.inject.Inject;
+
 import java.net.InetAddress;
 
 public class HomeController extends Controller {
+    private static final String FOOBAR_KEY = "net.codetojoy.foobar";
+
+    private final Config config;
+
+    @Inject
+    public HomeController(Config config) {
+        this.config = config;
+    }
 
     public Result index(Http.Request request) {
         String timestamp = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
         String clientIpAddress = request.remoteAddress();
         String serverIpAddress = getServerIpAddress();
-        return ok(views.html.main.render(timestamp, clientIpAddress, serverIpAddress));
+        String foobarValue = getConfigValue(FOOBAR_KEY);
+        return ok(views.html.main.render(timestamp, clientIpAddress, serverIpAddress, foobarValue));
     }
+
+    public Result health() {
+        return ok("health ok");
+    }
+
+    // ------------------------------------------
 
     private String getServerIpAddress() {
         try {
@@ -21,7 +39,11 @@ public class HomeController extends Controller {
         }
     }
 
-    public Result health() {
-        return ok("health ok");
+    private String getConfigValue(String key) {
+        if (config.hasPath(key)) {
+            return config.getString(key);
+        } else {
+            return "no such key";
+        }
     }
 }
